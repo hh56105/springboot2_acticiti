058b36e5f87adb7f52dcad92636dc05571b322a9
Coinfake <746425022@qq.com>
2021-03-31 21:49:44 +0800

    Initial commit

---
+++ childone/src/main/java/com/tsefuning/childone/ChildoneApplication.java
@@ -0,0 +1,13 @@
+package com.tsefuning.childone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildoneApplication.class, args);
+    }
+
+}
---
+++ childone/src/test/java/com/tsefuning/childone/ChildoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsefuning.childone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
